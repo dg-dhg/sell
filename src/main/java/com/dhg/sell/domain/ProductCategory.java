4157f7abba6f96517f69480abb20682d644764df
fyt3532698 <156612426@qq.com>
2020-01-16 11:09:10 +0800

    mock模拟客户端发送请求及响应测试1补丁

--- src/main/java/com/dhg/sell/domain/ProductCategory.java
+++ src/main/java/com/dhg/sell/domain/ProductCategory.java
@@ -7,9 +7,11 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @JsonPropertyOrder(value={"categoryId","categoryType"})
 public class ProductCategory{
